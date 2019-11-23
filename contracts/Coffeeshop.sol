a1add89fbbc90fc7b26bbebd8596b3b0f670103b
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2019-11-23 14:00:27 +0000

    Update Coffeeshop.sol

--- contracts/Coffeeshop.sol
+++ contracts/Coffeeshop.sol
@@ -71,13 +71,10 @@ modifier checkValue(uint _coffeeId) {
 }
 
 // Transfer the correct amount to seller (Bob) after checking if the coffeeId is valid, the buyer (Alice) has paid enough, and after send the change to buyer
-function buy(uint _coffeeId) public payable validId(_coffeeId) paidEnough(_coffeeId) checkValue(_coffeeId) returns(uint) {	
+function buy(uint _coffeeId) public payable validId(_coffeeId) paidEnough(_coffeeId) checkValue(_coffeeId) {	
 	(bool success, ) = bob.call.value(msg.value)("");
 	require(success);
-    
-	return _coffeeId;
+	
 }
-
-
 
 }
